5c8647c7a93966d428e304ecc894cff2359c133f
James Howard <jthoward09@gmail.com>
2021-01-28 22:05:58 -0500

    Create employee table

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -5,7 +5,16 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
+  department_id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (department_id)
 );
+
+CREATE TABLE employee (
+    employee_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (employee_id)
+)
