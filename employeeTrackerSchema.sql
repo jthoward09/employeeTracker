07515b5b28ab8b92189bdfd33184b9d20b7c8868
James Howard <jthoward09@gmail.com>
2021-01-28 22:00:38 -0500

    Create department table

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -3,3 +3,9 @@ DROP DATABASE IF EXISTS employee_DB;
 CREATE DATABASE employee_DB;
 
 USE employee_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
