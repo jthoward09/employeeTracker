03e37154e4c3c2b4bcf44f4952ac9e521dbbf322
James Howard <jthoward09@gmail.com>
2021-01-30 12:23:11 -0500

    Add values to the database

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -18,3 +18,29 @@ CREATE TABLE employee (
     manager_id INT NULL,
     PRIMARY KEY (employee_id)
 )
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,4),
+    department_id INT,
+)
+
+INSERT INTO department (name)
+VALUES('Engineering'), 
+('Sales'),
+('Legal'),
+('Design'),
+('Support');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Engineer II', 100000, 1),
+('Sales Manager', 70000, 2),
+('Sales Lead', 45000, 2),
+('Creative Director', 90000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Smith', 1),
+('Betty', 'White', 2),
+('Shawn', 'Connery', 3, 1),
+('Micheal', 'Jordan', 4);
